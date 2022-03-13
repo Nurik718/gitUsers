f715b19d426d31b6dfa6eb4dc2de8712aebdc626
Nurik718 <nurakamikg@gmail.com>
2022-03-13 21:52:26 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const github = new Github();
+const ui = new UI();
+
+const searchInput = document.getElementById("search-user");
+
+searchInput.addEventListener("keyup", (e) => {
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        ui.showAlert("User Not Found! Sorry!", "alert alert-danger");
+      } else {
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    ui.clearProfile();
+  }
+});
